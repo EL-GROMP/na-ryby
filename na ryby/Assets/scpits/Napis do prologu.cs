aeb0aef325b9573db84de030f638ca7ef0597b09
PPfxd <ppfxd.nieznany@gmail.com>
2023-08-22 12:56:10 +0200

    rtyu

--- na ryby/Assets/scpits/Napis do prologu.cs
+++ na ryby/Assets/scpits/Napis do prologu.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Threading;
+using System;
 
 public class Napisdoprologu : MonoBehaviour
 {
